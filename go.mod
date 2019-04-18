de8436843ad0a82bfeffae33adb3be6571f5a4d4
lianyue <admin@lianyue.org>
2019-04-18 22:49:30 +0800

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/otamoe/storage-model
+
+require (
+	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
+	github.com/otamoe/gin-server v0.0.0-20190417131835-16f0a3f7019d
+	github.com/otamoe/mgo-model v0.0.0-20190412052249-cb4bbacc4d83
+	github.com/sirupsen/logrus v1.4.1
+)
---
+++ storage.go
@@ -0,0 +1,182 @@
+package model
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"path"
+	"strings"
+	"time"
+
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+	"github.com/otamoe/gin-server/errs"
+	mgoModel "github.com/otamoe/mgo-model"
+	"github.com/sirupsen/logrus"
+)
+
+var STORAGE = "/"
+var STORAGE_PATH = "/"
+
+type (
+	Storage struct {
+		mgoModel.DocumentBase `json:"-" bson:"-" binding:"-"`
+
+		Unique string `json:"unique" bson:"unique" binding:"required"`
+
+		Path string `json:"path" bson:"path" binding:"required"`
+
+		HLS    string `json:"hls,omitempty" bson:"hls,omitempty"`
+		HLSKey string `json:"hls_key,omitempty" bson:"hls_key,omitempty"`
+
+		Status  string `json:"status,omitempty" bson:"status" binding:"required,oneof=pending approved banned"`
+		Name    string `json:"name,omitempty" bson:"name" binding:"required,max=512"`
+		Type    string `json:"type,omitempty" bson:"type" binding:"omitempty,max=32"`
+		SubType string `json:"sub_type,omitempty" bson:"sub_type" binding:"omitempty,max=64"`
+
+		Size     int64                  `json:"size,omitempty" bson:"size" binding:"required,min=1"`
+		Duration float64                `json:"duration,omitempty" bson:"duration,omitempty" binding:"omitempty,min=0,max=2592000"`
+		Width    int                    `json:"width,omitempty" bson:"width,omitempty" binding:"omitempty,min=0,max=32767"`
+		Height   int                    `json:"height,omitempty" bson:"height,omitempty" binding:"omitempty,min=0,max=32767"`
+		Pixels   int                    `json:"pixels,omitempty" bson:"pixels,omitempty" binding:"omitempty,min=0,max=268435456"`
+		Meta     map[string]interface{} `json:"meta,omitempty" bson:"meta,omitempty"`
+
+		Complete bool `json:"complete,omitempty" bson:"complete"`
+
+		CreatedAt *time.Time `json:"created_at,omitempty" bson:"created_at" binding:"required"`
+		UpdatedAt *time.Time `json:"updated_at,omitempty" bson:"updated_at" binding:"required"`
+		DeletedAt *time.Time `json:"deleted_at,omitempty" bson:"deleted_at,omitempty"`
+
+		Errors     []*errs.Error `json:"errors,omitempty" bson:"errors,omitempty"`
+		StatusCode int           `json:"status_code,omitempty" bson:"status_code,omitempty"`
+	}
+)
+
+var (
+	ErrStorageNotFound error = &errs.Error{
+		Message:    "File not found",
+		Path:       "storage",
+		Type:       "not_found",
+		StatusCode: http.StatusNotFound,
+	}
+
+	ModelStorage = &mgoModel.Model{
+		Name:     "storages",
+		Document: &Storage{},
+		Indexs: []mgo.Index{
+			mgo.Index{
+				Key:        []string{"unique"},
+				Unique:     true,
+				Background: true,
+			},
+		},
+	}
+)
+
+func Get(ctx context.Context, val string, cache bool, save bool) (storage *Storage, err error) {
+	val2 := strings.Split(val, "/")
+	var base string
+	if len(val2) == 2 && bson.IsObjectIdHex(val2[0]) && bson.IsObjectIdHex(val2[1]) {
+		base = STORAGE
+	} else {
+		base = STORAGE_PATH
+		for _, val := range val2 {
+			if val == "" || strings.TrimSpace(val) != val || val[0] == '.' || strings.ContainsAny(val, "/:*?#%&<>\\") {
+				err = ErrStorageNotFound
+				return
+			}
+		}
+	}
+	if cache {
+		storage = &Storage{}
+		if err = ModelStorage.Query(ctx).Eq("unique", val).One(storage); err != mgo.ErrNotFound {
+			return
+		}
+	}
+
+	if storage.Unique == "" {
+		storage = fetch(path.Join(base, val) + "/")
+		storage.Unique = val
+	}
+
+	if save {
+		var isNew bool
+		if cache {
+			isNew = true
+		} else if err = ModelStorage.Query(ctx).Eq("unique", val).One(storage); err == mgo.ErrNotFound {
+			isNew = true
+		}
+		storage.New(ctx, ModelStorage, storage, isNew)
+	}
+	return
+}
+
+func fetch(url string) (storage *Storage) {
+	var err error
+	storage = &Storage{}
+
+	defer func() {
+		if err == nil {
+			return
+		}
+		var ginErr *errs.Error
+		switch err.(type) {
+		case *errs.Error:
+			ginErr = err.(*errs.Error)
+		default:
+			ginErr = &errs.Error{
+				Err: err,
+			}
+		}
+
+		storage.Errors = append(storage.Errors, ginErr)
+		if storage.StatusCode != 0 {
+
+		} else if ginErr.StatusCode != 0 {
+			storage.StatusCode = ginErr.StatusCode
+		} else {
+			storage.StatusCode = http.StatusInternalServerError
+		}
+	}()
+	var res *http.Response
+	var bodyBytes []byte
+
+	client := &http.Client{}
+
+	timeoutCtx, timeoutCancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer timeoutCancel()
+
+	var req *http.Request
+	if req, err = http.NewRequest("GET", url, nil); err != nil {
+		err = ErrStorageNotFound
+		return
+	}
+	req = req.WithContext(timeoutCtx)
+	if res, err = client.Do(req); err != nil {
+		return
+	}
+	defer res.Body.Close()
+	if bodyBytes, err = ioutil.ReadAll(res.Body); err != nil {
+		return
+	}
+
+	logrus.Debugf("[Storage] %d %s", res.StatusCode, string(bodyBytes))
+
+	if res.StatusCode >= 500 {
+		err = &errs.Error{
+			Message:    "Storage: Status code error",
+			StatusCode: res.StatusCode,
+		}
+		return
+	}
+	if res.StatusCode > 200 {
+		err = ErrStorageNotFound
+		return
+	}
+
+	if err = json.Unmarshal(bodyBytes, storage); err != nil {
+		return
+	}
+	return
+}
